22e1adf3fdd6ab7d41fc5fcc78139a7f23b5fd5d
Reto Buerki <reet@codelabs.ch>
2012-02-27 12:50:02 +0100

    Sockets: Add No_Addr constant

--- src/anet-sockets.ads
+++ src/anet-sockets.ads
@@ -83,6 +83,8 @@ package Anet.Sockets is
    --  family-specific address information such as the IP address and port of a
    --  sender of data.
 
+   No_Addr : constant Socket_Addr_Type;
+
    function To_String (Address : Socket_Addr_Type) return String;
    --  Return string representation for given socket address type.
 
@@ -213,6 +215,9 @@ package Anet.Sockets is
    Socket_Error : exception;
 
 private
+
+   No_Addr : constant Socket_Addr_Type := (Family => Family_Inet,
+                                           others => <>);
 
    type Socket_Type is new Ada.Finalization.Limited_Controlled with record
       Sock_FD : Integer := -1;
