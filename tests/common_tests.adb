435e0bbb056b0bff9c745f2028c91362492ddc1e
Reto Buerki <reet@codelabs.ch>
2014-03-13 18:18:35 +0100

    Add Common_Tests package
    The Add procedure provided by this package can be used to register
    testcases common to all supported operating systems.

---
+++ tests/common_tests.adb
@@ -0,0 +1,62 @@
+--
+--  Copyright (C) 2014 Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014 Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software; you can redistribute it and/or modify it
+--  under the terms of the GNU General Public License as published by the
+--  Free Software Foundation; either version 2 of the License, or (at your
+--  option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+--
+--  This program is distributed in the hope that it will be useful, but
+--  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+--  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+--  for more details.
+--
+--  As a special exception, if other files instantiate generics from this
+--  unit,  or  you  link  this  unit  with  other  files  to  produce  an
+--  executable   this  unit  does  not  by  itself  cause  the  resulting
+--  executable to  be  covered by the  GNU General  Public License.  This
+--  exception does  not  however  invalidate  any  other reasons why  the
+--  executable file might be covered by the GNU Public License.
+--
+
+with Type_Tests;
+with OS_Tests;
+with Util_Tests;
+with Socket_Tests.IP;
+with Socket_Tests.Unix;
+with UDP_Tests;
+with IP_Tests;
+with Stream_Tests;
+with Net_Ifaces_Tests;
+
+package body Common_Tests is
+
+   use Ahven.Framework;
+
+   -------------------------------------------------------------------------
+
+   procedure Add (Suite : Test_Suite_Access)
+   is
+   begin
+      Add_Test (Suite => Suite.all,
+                T     => new Type_Tests.Testcase);
+      Add_Test (Suite => Suite.all,
+                T     => new OS_Tests.Testcase);
+      Add_Test (Suite => Suite.all,
+                T     => new Util_Tests.Testcase);
+      Add_Test (Suite => Suite.all,
+                T     => new Socket_Tests.IP.Testcase);
+      Add_Test (Suite => Suite.all,
+                T     => new Socket_Tests.Unix.Testcase);
+      Add_Test (Suite => Suite.all,
+                T     => new UDP_Tests.Testcase);
+      Add_Test (Suite => Suite.all,
+                T     => new IP_Tests.Testcase);
+      Add_Test (Suite => Suite.all,
+                T     => new Stream_Tests.Testcase);
+      Add_Test (Suite => Suite.all,
+                T     => new Net_Ifaces_Tests.Testcase);
+   end Add;
+
+end Common_Tests;
